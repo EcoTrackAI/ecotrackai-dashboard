6b02ca8ced42f4336397b7c58c0e71b7b037eace
sayam-1705 <sayam2022mondal@gmail.com>
2025-12-16 12:35:39 +0530

    Replace sensitive informations with env values

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyClsDln-Zx961zC0f7X0-C2wJ9GCrwp46A",
-  authDomain: "ecotrackai-7a140.firebaseapp.com",
-  databaseURL:
-    "https://ecotrackai-7a140-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "ecotrackai-7a140",
-  storageBucket: "ecotrackai-7a140.firebasestorage.app",
-  messagingSenderId: "307182555730",
-  appId: "1:307182555730:web:a137c7a1ef5a427d80058b",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
